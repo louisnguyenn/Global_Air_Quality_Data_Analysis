213afcda7807469401466b451c590adc3a3b525f
louis nguyen <louis.nguyen550@gmail.com>
2025-06-23 17:37:30 -0400

    creating output file for preprocessing

--- air_quality_script.py
+++ air_quality_script.py
@@ -4,7 +4,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
-def main()
+def main():
+
+    # reading the dataset using pandas csv reader
+    df = pd.read_csv("air_quality_health_dataset.csv")
+
     plotly_fig = px.bar(df, x="city", y="aqi", title="Air Quality Metrics in Cities")
     plotly_fig.show()
 
--- data_preprocess.py
+++ data_preprocess.py
@@ -1,10 +1,28 @@
-import sys
 import pandas as pd
 
-def main()
-    # reading the dataset using pandas csv reader
-    df = pd.read_csv("air_quality_health_dataset.csv")
-    # display(df)
+def main():
 
-    
+    # open an output file to use for data analysis in write mode
+    try:
+        output_dataset = open("air_quality_preprocessed_dataset", "w", newline='', encoding="utf-8-sig")
 
+    except IOError as err:
+        print("Unable to open output file 'air_quality_preprocessed_dataset' : {}".format(err), file=sys.stderr)
+        sys.exit(1)
+
+    # reading the dataset using a csv reader
+    data = pd.read_csv("air_quality_health_dataset.csv")
+
+    # skipping the header of the csv file
+    next(dataset_reader, None)
+
+    # creating a variable to write onto the output file
+    dataset_writer = csv.writer(air_quality_preprocessed_dataset)
+
+    # pre process the required data from the csv
+    for row in data:
+        city = row[0]
+        air_quality = row[2]
+
+        if city == "Delhi":
+            
