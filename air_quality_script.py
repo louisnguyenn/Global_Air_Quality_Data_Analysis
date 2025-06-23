72e3c4f958bcccc2541cc7454805640fc15ec007
louis nguyen <louis.nguyen550@gmail.com>
2025-06-23 17:59:16 -0400

    adding preprocessed file

--- air_quality_script.py
+++ air_quality_script.py
@@ -3,14 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+import sys
 
 def main():
 
-    # reading the dataset using pandas csv reader
-    df = pd.read_csv("air_quality_health_dataset.csv")
+    # reading the dataset as a dataframe using pandas using try-except block
+    try: 
+        df = pd.read_csv("air_quality_preprocessed_dataset.csv", encoding="utf-8-sig")
+    
+    except IOError as err:
+        print("Unable to open pre-processed file 'air_quality_preprocessed_dataset.csv' : {}".format(err), file=sys.stderr)
+        sys.exit(1)
 
+    # using plotly to create a bar graph
     plotly_fig = px.bar(df, x="city", y="aqi", title="Air Quality Metrics in Cities")
     plotly_fig.show()
 
-    print("Column names:")
-    print(df.columns.tolist())
+main()
