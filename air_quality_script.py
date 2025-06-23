a6ec366da2a286ecb71c69cd54bf34e2e4d0d756
louis nguyen <louis.nguyen550@gmail.com>
2025-06-23 17:19:41 -0400

    creating preprocessing file

--- air_quality_script.py
+++ air_quality_script.py
@@ -2,8 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
-# reading the dataset using pandas csv reader
-df = pd.read_csv("air_quality_health_dataset.csv")
-# display(df)
+def main()
+    plotly_fig = px.bar(df, x="city", y="aqi", title="Air Quality Metrics in Cities")
+    plotly_fig.show()
 
+    print("Column names:")
+    print(df.columns.tolist())
---
+++ data_preprocess.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+
+def main()
+    # reading the dataset using pandas csv reader
+    df = pd.read_csv("air_quality_health_dataset.csv")
+    # display(df)
+
+    
+
